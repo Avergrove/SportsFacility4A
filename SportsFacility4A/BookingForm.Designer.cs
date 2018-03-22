c27ad366b8f4473738f042f2d61178f1e06fe4ca
Saisruthi Puppala <pssruthi123@gmail.com>
2018-03-22 23:10:02 +0800

    re

--- SportsFacility4A/BookingForm.Designer.cs
+++ SportsFacility4A/BookingForm.Designer.cs
@@ -36,8 +36,6 @@
 			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
 			this.label2 = new System.Windows.Forms.Label();
 			this.comboBox2 = new System.Windows.Forms.ComboBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.searchDataGrid)).BeginInit();
 			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
@@ -126,31 +124,11 @@
 			this.comboBox2.Size = new System.Drawing.Size(211, 28);
 			this.comboBox2.TabIndex = 7;
 			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(34, 71);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 10;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(272, 43);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
-			this.button3.TabIndex = 11;
-			this.button3.Text = "button3";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
 			// BookingForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1110, 752);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
 			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.searchDataGrid);
 			this.Controls.Add(this.comboBox2);
@@ -180,7 +158,5 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox2;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
 	}
 }
