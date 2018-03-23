81968e869ed71e4140b6f78a094118513a31a972
Saisruthi Puppala <pssruthi123@gmail.com>
2018-03-23 10:38:09 +0800

    Added Venue Maintenance Screen

---
+++ SportsFacility4A/Venue Maintenance Screen.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SportsFacility4A
+{
+	partial class Venue_Maintenance_Screen
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.venuelabel = new System.Windows.Forms.Label();
+			this.venueidlabel = new System.Windows.Forms.Label();
+			this.venuenamelabel = new System.Windows.Forms.Label();
+			this.Venueaddresslabel = new System.Windows.Forms.Label();
+			this.venuecategorylabel = new System.Windows.Forms.Label();
+			this.venueidtextbox = new System.Windows.Forms.TextBox();
+			this.venuenametextbox = new System.Windows.Forms.TextBox();
+			this.venueaddresstextbox = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.updatebutton1 = new System.Windows.Forms.Button();
+			this.createbutton = new System.Windows.Forms.Button();
+			this.searchbutton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// venuelabel
+			// 
+			this.venuelabel.AutoSize = true;
+			this.venuelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.venuelabel.Location = new System.Drawing.Point(255, 64);
+			this.venuelabel.Name = "venuelabel";
+			this.venuelabel.Size = new System.Drawing.Size(433, 37);
+			this.venuelabel.TabIndex = 0;
+			this.venuelabel.Text = "Venue Maintenance Screen";
+			// 
+			// venueidlabel
+			// 
+			this.venueidlabel.AutoSize = true;
+			this.venueidlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.venueidlabel.Location = new System.Drawing.Point(299, 160);
+			this.venueidlabel.Name = "venueidlabel";
+			this.venueidlabel.Size = new System.Drawing.Size(94, 25);
+			this.venueidlabel.TabIndex = 1;
+			this.venueidlabel.Text = "Venue ID";
+			// 
+			// venuenamelabel
+			// 
+			this.venuenamelabel.AutoSize = true;
+			this.venuenamelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.venuenamelabel.Location = new System.Drawing.Point(287, 240);
+			this.venuenamelabel.Name = "venuenamelabel";
+			this.venuenamelabel.Size = new System.Drawing.Size(127, 25);
+			this.venuenamelabel.TabIndex = 2;
+			this.venuenamelabel.Text = "Venue Name";
+			// 
+			// Venueaddresslabel
+			// 
+			this.Venueaddresslabel.AutoSize = true;
+			this.Venueaddresslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Venueaddresslabel.Location = new System.Drawing.Point(267, 327);
+			this.Venueaddresslabel.Name = "Venueaddresslabel";
+			this.Venueaddresslabel.Size = new System.Drawing.Size(148, 25);
+			this.Venueaddresslabel.TabIndex = 3;
+			this.Venueaddresslabel.Text = "Venue Address";
+			// 
+			// venuecategorylabel
+			// 
+			this.venuecategorylabel.AutoSize = true;
+			this.venuecategorylabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.venuecategorylabel.Location = new System.Drawing.Point(301, 432);
+			this.venuecategorylabel.Name = "venuecategorylabel";
+			this.venuecategorylabel.Size = new System.Drawing.Size(92, 25);
+			this.venuecategorylabel.TabIndex = 4;
+			this.venuecategorylabel.Text = "Category";
+			// 
+			// venueidtextbox
+			// 
+			this.venueidtextbox.Location = new System.Drawing.Point(454, 159);
+			this.venueidtextbox.Name = "venueidtextbox";
+			this.venueidtextbox.Size = new System.Drawing.Size(100, 26);
+			this.venueidtextbox.TabIndex = 5;
+			// 
+			// venuenametextbox
+			// 
+			this.venuenametextbox.Location = new System.Drawing.Point(454, 223);
+			this.venuenametextbox.Multiline = true;
+			this.venuenametextbox.Name = "venuenametextbox";
+			this.venuenametextbox.Size = new System.Drawing.Size(309, 56);
+			this.venuenametextbox.TabIndex = 6;
+			// 
+			// venueaddresstextbox
+			// 
+			this.venueaddresstextbox.Location = new System.Drawing.Point(454, 316);
+			this.venueaddresstextbox.Multiline = true;
+			this.venueaddresstextbox.Name = "venueaddresstextbox";
+			this.venueaddresstextbox.Size = new System.Drawing.Size(309, 60);
+			this.venueaddresstextbox.TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(454, 417);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(221, 50);
+			this.textBox4.TabIndex = 8;
+			// 
+			// updatebutton1
+			// 
+			this.updatebutton1.Location = new System.Drawing.Point(545, 542);
+			this.updatebutton1.Name = "updatebutton1";
+			this.updatebutton1.Size = new System.Drawing.Size(163, 59);
+			this.updatebutton1.TabIndex = 9;
+			this.updatebutton1.Text = "Update";
+			this.updatebutton1.UseVisualStyleBackColor = true;
+			// 
+			// createbutton
+			// 
+			this.createbutton.Location = new System.Drawing.Point(272, 542);
+			this.createbutton.Name = "createbutton";
+			this.createbutton.Size = new System.Drawing.Size(143, 59);
+			this.createbutton.TabIndex = 10;
+			this.createbutton.Text = "Create";
+			this.createbutton.UseVisualStyleBackColor = true;
+			// 
+			// searchbutton
+			// 
+			this.searchbutton.Location = new System.Drawing.Point(582, 160);
+			this.searchbutton.Name = "searchbutton";
+			this.searchbutton.Size = new System.Drawing.Size(51, 24);
+			this.searchbutton.TabIndex = 11;
+			this.searchbutton.Text = "...";
+			this.searchbutton.UseVisualStyleBackColor = true;
+			// 
+			// Venue_Maintenance_Screen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(871, 665);
+			this.Controls.Add(this.searchbutton);
+			this.Controls.Add(this.createbutton);
+			this.Controls.Add(this.updatebutton1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.venueaddresstextbox);
+			this.Controls.Add(this.venuenametextbox);
+			this.Controls.Add(this.venueidtextbox);
+			this.Controls.Add(this.venuecategorylabel);
+			this.Controls.Add(this.Venueaddresslabel);
+			this.Controls.Add(this.venuenamelabel);
+			this.Controls.Add(this.venueidlabel);
+			this.Controls.Add(this.venuelabel);
+			this.Name = "Venue_Maintenance_Screen";
+			this.Text = "Venue_Maintenance_Screen";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label venuelabel;
+		private System.Windows.Forms.Label venueidlabel;
+		private System.Windows.Forms.Label venuenamelabel;
+		private System.Windows.Forms.Label Venueaddresslabel;
+		private System.Windows.Forms.Label venuecategorylabel;
+		private System.Windows.Forms.TextBox venueidtextbox;
+		private System.Windows.Forms.TextBox venuenametextbox;
+		private System.Windows.Forms.TextBox venueaddresstextbox;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button updatebutton1;
+		private System.Windows.Forms.Button createbutton;
+		private System.Windows.Forms.Button searchbutton;
+	}
+}
