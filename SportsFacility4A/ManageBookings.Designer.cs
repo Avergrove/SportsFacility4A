f57b3e3bbcbc6e55467213e69d5910a174479c23
Esther Hoon <e0283990@u.nus.edu>
2018-03-23 11:24:22 +0800

    managebookings

--- SportsFacility4A/ManageBookings.Designer.cs
+++ SportsFacility4A/ManageBookings.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SportsFacility4A
 {
-    partial class Form1
+    partial class Managebookings
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Managebookings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Managebookings";
+            this.Text = "Manage Bookings";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SportsFacility4A/ManageBookings.cs
+++ SportsFacility4A/ManageBookings.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SportsFacility4A
 {
-    public partial class Form1 : Form
+    public partial class Managebookings : Form
     {
-        public Form1()
+        public Managebookings()
         {
             InitializeComponent();
         }
